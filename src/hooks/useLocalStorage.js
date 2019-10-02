4d5cd0bccd6e9899af8599ab91feac73a53634e9
Devin Warrick <dev.warr@gmail.com>
2019-10-02 12:41:47 -0500

    Change ternary into if statement

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,13 +1,21 @@
 import { useState } from "react";
 
 export default function useLocalStorage(key, initialValue) {
-    const [storedValue, setStoredValue] = useState(() => {
-        const value = window.localStorage.getItem(key);
-        return value ? JSON.parse(value) : initialValue
-    });
-    const saveToStorage = value => {
-        window.localStorage.setItem(key, JSON.stringify(value));
-        setStoredValue(value);
+
+  const [storedValue, setStoredValue] = useState(() => {
+    const value = window.localStorage.getItem(key);
+    if (value) {
+      return JSON.parse(value);
+    } else {
+      localStorage.setItem(key, JSON.parse(initialValue));
+      return initialValue;
     }
-    return [storedValue, saveToStorage];
+  });
+
+  const saveToStorage = value => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+    setStoredValue(value);
+  };
+
+  return [storedValue, saveToStorage];
 }
