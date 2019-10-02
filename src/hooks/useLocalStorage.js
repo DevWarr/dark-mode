4e7cf1b51af8de4b48f66665cd083229bd94fc66
Devin Warrick <dev.warr@gmail.com>
2019-10-02 12:52:59 -0500

    Add Error handling and logic to useLocalStorage.js

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,6 +2,14 @@ import { useState } from "react";
 
 export default function useLocalStorage(key, initialValue) {
 
+    // Error handling: if key is not a string we will throw an error
+    if (typeof key !== "string") {
+        let errorMessage = "Invalid Params: useLocalStorage(key, initialValue)\n"
+        errorMessage += `expected 'key' to be type 'string'; received '${typeof key}'.`
+        throw new Error(errorMessage)
+    }
+
+  // using state to set our initial value IF local storage has it
   const [storedValue, setStoredValue] = useState(() => {
     const value = window.localStorage.getItem(key);
     if (value) {
@@ -12,10 +20,14 @@ export default function useLocalStorage(key, initialValue) {
     }
   });
 
+  // Creating a function that uses our state above to save to localStorage 
+  //     AND update our current value
   const saveToStorage = value => {
     window.localStorage.setItem(key, JSON.stringify(value));
     setStoredValue(value);
   };
 
+  // return our variable, 
+  //     and the function to properly update our variable with localStorage
   return [storedValue, saveToStorage];
 }
